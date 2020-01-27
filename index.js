e682f0329acf3ef5ca3ca7a6a440dde1fbd1ed8e
James Stamps Jr <jstampshere@gmail.com>
2020-01-27 04:09:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ function happyHolidays (string) {
   return `Happy holidays!`
 }
 
-function happyHolidaysTo `(string) + ${name}` {
+function happyHolidaysTo (string) {
   return `Happy holidays, ${James}!`
 }
